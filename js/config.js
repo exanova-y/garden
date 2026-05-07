dca75e2d584bea1507e492d9f09e4f72a85ad4f1
Yoyo Yuan <yoyoyuan1729@gmail.com>
2026-05-06 22:28:06 -0700

    (not working yet)

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // Site-wide configuration variables
 const siteConfig = {
     // Basic site information
-    siteName: "Yoyo cat!",
-    siteTagline: "L-glutamic acid's homepage!",
-    copyright: "2025 Yoyo!",
+    siteName: "Yoyo",
+    siteTagline: "adiabatic.garden",
+    copyright: "by Yoyo from Pawsitive Feedback (out of) ctrl!",
     
     // Author information
     author: "Yoyo",
@@ -11,7 +11,7 @@ const siteConfig = {
     // Social links
     socialLinks: {
         github: "https://github.com/exanova-y",
-        twitter: "https://twitter.com/c5h9no4_o"
+        twitter: "https://twitter.com/adiabaticgarden"
     },
     
     // Discord ID for status (find yours at https://www.google.com/search?q=how+to+find+discord+id)
@@ -22,7 +22,5 @@ const siteConfig = {
         { name: "home", url: "/" },
         { name: "readlog", url: "/readlog.html" },
         { name: "writing", url: "/writing.html" },
-        { name: "logs", url: "/logs.html"},
-        { name: "vibes", url: "https://yoyo.cat"}
     ]
 };
